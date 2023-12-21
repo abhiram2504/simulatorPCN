18c3bf834e86b10b40003f466865346e1913cf56
abhiram2504 <abhiram.rustagi@gmail.com>
2023-12-20 22:54:29 -0500

    added alternative routings

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-'''
-This uses a random graph
-num_nodes = 10
-#this is the probability between an edge
-probability = 0.4
-G_temp = nx.random_graphs.erdos_renyi_graph(num_nodes, probability)
-
-Goal is to check how we can use comman randomesses for routing. 
-
-Demand matrix. How much each person wants to send to another perosn.
-
-Look into routing policies. Measeure the success rate of transactions. 
-'''
-
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
+def try_alternative_paths(G, src, dest, transaction_amt):
+    # Try the shortest path as an alternative
+    shortest_path = nx.shortest_path(G, source=src, target=dest, weight='weight')
+    feasible_transaction = all(G[u][v]['weight'] >= transaction_amt for u, v in zip(shortest_path, shortest_path[1:]))
+    if feasible_transaction:
+        for u, v in zip(shortest_path, shortest_path[1:]):
+            G[u][v]['weight'] -= transaction_amt
+        return True
+    else:
+        print("Shortest path transaction failed due to insufficient capacity.")
+        return False
+
 # Use a pre-built weighted graph (karate club graph in this case)
-#this is an inbuild grapgh
 G = nx.karate_club_graph()
 
 # Add random weights to edges to represent channel capacities
@@ -33,26 +30,24 @@ for transaction in range(num_transactions):
     dest = random.choice(list(G.nodes))
     print(f"\nTransaction {transaction + 1}: The src is {src}, and the dest is {dest}.")
 
-    # Shortest path b/w src and dest
-    shortest_path = nx.shortest_path(G, source=src, target=dest, weight='weight')
+    # First, try the widest path
+    paths = nx.all_simple_paths(G, source=src, target=dest)
+    widest_path = max(paths, key=lambda path: min(G[u][v]['weight'] for u, v in zip(path, path[1:])), default=None)
 
-    transaction_amt = random.randint(0,10)
+    # If the widest path is found and feasible, use it
+    if widest_path:
+        transaction_amt = random.randint(0, 10)
+        if all(G[u][v]['weight'] >= transaction_amt for u, v in zip(widest_path, widest_path[1:])):
+            for u, v in zip(widest_path, widest_path[1:]):
+                G[u][v]['weight'] -= transaction_amt
+        else:
+            print("Widest path transaction failed due to insufficient capacity.")
+            # Try alternative paths
+            if not try_alternative_paths(G, src, dest, transaction_amt):
+                print("All transaction attempts failed.")
 
-    # Check if transaction is feasible based on channel capacities
-    feasible_transaction = all(G[u][v]['weight'] >= transaction_amt for u, v in zip(shortest_path, shortest_path[1:]))
-
-    if feasible_transaction:
-        # Update balance for the edge between src and dest
-        for u, v in zip(shortest_path, shortest_path[1:]):
-            G[u][v]['weight'] -= transaction_amt
-
-       
-
-    else:
-        print("Transaction failed due to insufficient capacity.")
-
- # Display the updated capacities on the edges
-print("Capacities after transaction:")
+# Display the updated capacities on the edges
+print("\nCapacities after transactions:")
 for u, v in G.edges():
     capacity = G[u][v]['weight']
     print(f"Edge ({u}, {v}): {capacity}")
@@ -61,4 +56,3 @@ for u, v in G.edges():
 nx.draw(G, with_labels=True)
 plt.title("Final Graph with Capacities")
 plt.show()
-
